9802503afe539e1c8664a48be641f5d884d696fb
dankuri <danek.il420@gmail.com>
2023-12-14 18:20:12 +0100

    day 7 part 2 + refactor

--- lib/day7.ex
+++ lib/day7.ex
@@ -1,6 +1,4 @@
 defmodule Day7 do
-  @card_power_p1 "AKQJT98765432" |> String.split("", trim: true)
-
   def part1(input) do
     input
     |> String.split("\n", trim: true)
@@ -12,9 +10,7 @@ defmodule Day7 do
       freq_arr =
         hand
         |> String.split("", trim: true)
-        |> Enum.reduce(%{}, fn char, appear_map ->
-          Map.update(appear_map, char, 1, &(&1 + 1))
-        end)
+        |> Enum.frequencies()
         |> Map.values()
         |> Enum.filter(&(&1 > 1))
         |> Enum.sort(&(&1 > &2))
@@ -32,12 +28,9 @@ defmodule Day7 do
 
       {hand, bid, hand_type}
     end)
-    |> Enum.sort(fn lhand, rhand ->
-      ltype = elem(lhand, 2)
-      rtype = elem(rhand, 2)
-
+    |> Enum.sort(fn {lcards, _, ltype}, {rcards, _, rtype} ->
       if ltype == rtype do
-        cards_better(elem(lhand, 0), elem(rhand, 0))
+        cards_better(lcards, rcards, :part_1)
       else
         ltype > rtype
       end
@@ -47,17 +40,88 @@ defmodule Day7 do
     |> Enum.reduce({0, 1}, fn bid, {acc, multiplier} ->
       {acc + bid * multiplier, multiplier + 1}
     end)
-    |> dbg()
+    |> elem(0)
   end
 
-  def cards_better("", ""), do: false
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn hand_bid ->
+      {List.first(hand_bid), List.last(hand_bid) |> String.to_integer()}
+    end)
+    |> Enum.map(fn {hand, bid} ->
+      freq_arr =
+        hand
+        |> String.split("", trim: true)
+        |> Enum.frequencies()
+        |> Enum.sort(fn {_, lfreq}, {_, rfreq} -> lfreq > rfreq end)
 
-  def cards_better(lcards, rcards) do
+      freq_arr =
+        case Enum.find(freq_arr, fn {card, _} -> card == "J" end) do
+          nil ->
+            freq_arr
+
+          {_, 5} ->
+            freq_arr
+
+          {_, jfreq} ->
+            Enum.reject(freq_arr, fn {card, _} -> card == "J" end)
+            |> List.update_at(0, fn {card, freq} -> {card, freq + jfreq} end)
+        end
+
+      hand_type =
+        case freq_arr |> Enum.map(&elem(&1, 1)) |> Enum.filter(&(&1 > 1)) do
+          [] -> 1
+          [2] -> 2
+          [2, 2] -> 3
+          [3] -> 4
+          [3, 2] -> 5
+          [4] -> 6
+          [5] -> 7
+        end
+
+      {hand, bid, hand_type}
+    end)
+    |> Enum.sort(fn lhand, rhand ->
+      {lcards, _, ltype} = lhand
+      {rcards, _, rtype} = rhand
+
+      if ltype == rtype do
+        cards_better(lcards, rcards, :part_2)
+      else
+        ltype > rtype
+      end
+    end)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.reverse()
+    |> Enum.reduce({0, 1}, fn bid, {acc, multiplier} ->
+      {acc + bid * multiplier, multiplier + 1}
+    end)
+    |> elem(0)
+  end
+
+  def cards_better("", "", _), do: false
+
+  @card_power_p1 "AKQJT98765432" |> String.split("", trim: true)
+  def cards_better(lcards, rcards, :part_1) do
     lcard_idx = Enum.find_index(@card_power_p1, &(&1 == String.at(lcards, 0)))
     rcard_idx = Enum.find_index(@card_power_p1, &(&1 == String.at(rcards, 0)))
 
     if lcard_idx == rcard_idx do
-      cards_better(String.slice(lcards, 1..-1), String.slice(rcards, 1..-1))
+      cards_better(String.slice(lcards, 1..-1), String.slice(rcards, 1..-1), :part_1)
+    else
+      lcard_idx < rcard_idx
+    end
+  end
+
+  @card_power_p2 "AKQT98765432J" |> String.split("", trim: true)
+  def cards_better(lcards, rcards, :part_2) do
+    lcard_idx = Enum.find_index(@card_power_p2, &(&1 == String.at(lcards, 0)))
+    rcard_idx = Enum.find_index(@card_power_p2, &(&1 == String.at(rcards, 0)))
+
+    if lcard_idx == rcard_idx do
+      cards_better(String.slice(lcards, 1..-1), String.slice(rcards, 1..-1), :part_2)
     else
       lcard_idx < rcard_idx
     end
